a7b0d0d3adadd4fe55edf01b41abed48f9d81684
Jacky Wu <JackyWuCoder@gmail.com>
2024-11-11 15:29:15 -0800

    Page Transitions Function (Active Button Class)
    1) In addEventListener, the first parameter "click" is the event type that defines how the second parameter of callback function will be called.
    2) We retrieve the current active button and if it is found then we remove the "active-btn" class name that defines this button.
    3) "this" keyword defines the html element that triggered this event/callback function, which is the button that was clicked. We add a class name of  "active-btn" to the clicked button.
    4) "this" keyword is different if the callback function is defined as a function () or an arrow function () => {}. If inside function() then "this" refers to the element that triggered the event but if you use arrow function "this" is defined as the elements that surround the clicked element instead.

---
+++ app.js
@@ -0,0 +1,22 @@
+const sections = document.querySelectorAll(".section");
+const sectBtns = document.querySelectorAll(".controls");
+const sectBtn = document.querySelectorAll(".control");
+const allSections = document.querySelector(".main-content");
+
+function PageTransitions() {
+  // Button click active class
+  for (let i = 0; i < sectBtn.length; i++) {
+    sectBtn[i].addEventListener("click", function () {
+      // Get Currently active button
+      let currentBtn = document.querySelectorAll(".active-btn");
+      // Remove 'active-btn' class from the previously active button
+      if (currentBtn.length > 0) {
+        currentBtn[0].classList.remove("active-btn");
+      }
+      // Add "active-btn" class to the clicked button
+      this.classList.add("active-btn");
+    });
+  }
+}
+
+PageTransitions();
