dea70b8be7d4b96c82c4d5fca9bd694eff29cb3e
Jacky Wu <JackyWuCoder@gmail.com>
2024-11-12 23:20:26 -0800

    Set Section's Class Name to Active

--- app.js
+++ app.js
@@ -18,9 +18,26 @@ function PageTransitions() {
     });
   }
 
-  // Sections Active class
+  // Sections Active
   allSections.addEventListener("click", (e) => {
-    console.log(e.target);
+    const id = e.target.dataset.id;
+    if (id) {
+      // remove selected from the other buttons
+      /*
+      sectBtns.forEach((btn) => {
+        btn.classList.remove("active");
+      });
+      e.target.classList.add("active");
+      */
+
+      // hide other sections
+      sections.forEach((section) => {
+        section.classList.remove("active");
+      });
+
+      const element = document.getElementById(id);
+      element.classList.add("active");
+    }
   });
 }
 
